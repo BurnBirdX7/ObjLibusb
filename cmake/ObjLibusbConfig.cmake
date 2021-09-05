53f84c205847212fbe9539ab87b539ed70ff9bec
burnbird <artemiy.lazarevx7@gmail.com>
2021-09-05 16:17:41 +0300

    Syntax error in -version file are fixed. -config file now tries to access Release configuration if ObjLibusb_REQUIRED_CONFIG and CMAKE_BUILD_TYPE are not defined (or set to empty string).

--- cmake/ObjLibusbConfig.cmake
+++ cmake/ObjLibusbConfig.cmake
@@ -7,51 +7,60 @@ get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 function(ObjLibusb_NoticeAvailableConfigurations)
 
-    set(types_str "")
-    foreach(type Unspecified;Debug;Release;RelWithDebInfo;MinSizeRel;Default)
+    set(found_configs)
+    foreach(type Debug;Release;RelWithDebInfo;MinSizeRel;Default)
 
         if (EXISTS ${SELF_DIR}/${type}/ObjLibusb.cmake)
-            set(types_str "${types_str}${type};")
+            list(APPEND found_configs ${type})
         endif()
 
     endforeach()
 
-    if (${types_str} STREQUAL "")
-        message(NOTICE "No installations of ObjLibusb found")
+    list(LENGTH found_configs found_count)
+    if ("${found_count}" EQUAL "0")
+        message(NOTICE "~~ No installations of ObjLibusb found")
+        set(example "Release")
     else()
-        message(NOTICE "Installations of ObjLibusb found:")
-        message(NOTICE "\t${types_str}")
+        message(NOTICE "~~ Found configurations of Objlibusb(${found_count}): ")
+        foreach(type ${found_configs})
+            message(NOTICE "~~\t${type}")
+        endforeach()
+
+        list(GET found_configs 0 example)
     endif()
 
-    message(NOTICE " -- Configurations with non-default names might not be displayed here -- ")
-    message(NOTICE "You can set required configurations via ObjLibusb_REQUIRED_CONFIG variable, for example: ")
-    message(NOTICE "\t# ...")
-    message(NOTICE "\tset(ObjLibusb_REQUIRED_CONFIG Release)")
-    message(NOTICE "\t# ...")
-
-    unset(types_str)
+    message(NOTICE "~~ * Configurations with non-standard names might not be listed")
+    message(NOTICE "~~ You can set desired configuration via ObjLibusb_REQUIRED_CONFIG variable, for example: ")
+    message(NOTICE "~~")
+    message(NOTICE "~~\tset(ObjLibusb_REQUIRED_CONFIG ${example})")
+    message(NOTICE "~~\tfind_package(ObjLibusb)")
+    message(NOTICE "~~")
+    message(NOTICE "~~ If user doesn't set ObjLibusb_REQUIRED_CONFIG then it is set to CMAKE_BUILD_TYPE, ")
+    message(NOTICE "~~\tif CMAKE_BUILD_TYPE is not defined (or is empty string) ObjLibusb_REQUIRED_CONFIG is set to 'Release'")
+    message(NOTICE "~~")
 
 endfunction()
 
 
 ### You can set ObjLibusb_REQUIRED_CONFIG to specify which config you want to install ###
-# ${CMAKE_BUILD_TYPE} will be used to search config if ObjLibusb_REQUIRED_CONFIG is not defined #
+# CMAKE_BUILD_TYPE will be used to search config if ObjLibusb_REQUIRED_CONFIG is not defined #
 
-if (DEFINED ${ObjLibusb_REQUIRED_CONFIG})
-    message("ObjLibusb_REQUIRED_CONFIG is set to: ${ObjLibusb_REQUIRED_CONFIG}")
-elseif(DEFINED ${CMAKE_BUILD_TYPE})
-    message("ObjLibusb_REQUIRED_CONFIG isn't set. Setting it to CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}...")
+
+if (DEFINED ObjLibusb_REQUIRED_CONFIG AND NOT ${ObjLibusb_REQUIRED_CONFIG} STREQUAL "")
+    message(NOTICE "~~ ObjLibusb_REQUIRED_CONFIG is set to: ${ObjLibusb_REQUIRED_CONFIG}.")
+elseif(DEFINED CMAKE_BUILD_TYPE AND NOT ${CMAKE_BUILD_TYPE} STREQUAL "")
+    message(NOTICE "~~ ObjLibusb_REQUIRED_CONFIG is not set. Setting it to CMAKE_BUILD_TYPE: '${CMAKE_BUILD_TYPE}'.")
     set(ObjLibusb_REQUIRED_CONFIG ${CMAKE_BUILD_TYPE})
 else()
-    message(SEND_ERROR "Neither ObjLibusb_REQUIRED_CONFIG nor CMAKE_BUILD_TYPE are set... Cannot proceed.")
+    message(NOTICE "~~ Neither ObjLibusb_REQUIRED_CONFIG nor CMAKE_BUILD_TYPE are set. Setting ObjLibusb_REQUIRED_CONFIG to 'Release'.")
+    set(ObjLibusb_REQUIRED_CONFIG Release)
 endif()
 
 
-if (EXISTS ${SELF_DIR}/${ObjLibusb_REQUIRED_CONFIG}/ObjLibusb.cmake)
+if (DEFINED ObjLibusb_REQUIRED_CONFIG AND EXISTS ${SELF_DIR}/${ObjLibusb_REQUIRED_CONFIG}/ObjLibusb.cmake)
     include(${SELF_DIR}/${ObjLibusb_REQUIRED_CONFIG}/ObjLibusb.cmake)
-    message(STATUS "ObjLibusb.cmake included")
 else()
-    message(NOTICE "Cannot include required configuration...")
+    message(NOTICE "~~ Required configuration does not exist")
     ObjLibusb_NoticeAvailableConfigurations()
-    message(SEND_ERROR "Cannot proceed.")
+    message(SEND_ERROR "[ObjLibusb] Unavailable configuration was requested")
 endif()
